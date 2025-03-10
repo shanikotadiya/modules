579586c70ed12ba613f70b3f762b03f8e82f9aaf
Shani Kotadiya <shanikotadiya@gmail.com>
2025-03-10 16:56:47 +0530

    uri change

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb+srv://shanikotadiya:HSKzVHRRVt5tqmeN@mymodules.qssso.mongodb.net/userdata"
+const MONGO_URI = process.env.MONGO_URI
 
 if (!MONGO_URI){
     throw new Error('Please define the MONGO_URI environment variable inside .env')
